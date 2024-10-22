c5c9cc8376c36c92b2c5fc85dab5533f7ac1b519
Juan Novas <juanignacionovas@gmail.com>
2024-10-22 09:09:42 -0300

    bool() constructor added

---
+++ bi_functions/bool.py
@@ -0,0 +1,17 @@
+# Takes an OBJ and returns TRUE or FALSE
+
+bool("aaa") # Returns True
+
+bool("") # Returns False
+
+bool() # Returns False
+
+bool(1) # Returns True
+
+bool(0) # Returns False
+
+bool([]) # Returns False
+
+bool([[]]) # Return True
+
+bool(bool) # Returns True
