db4e0027c5e9840ea4eead8afc13c2fefe65f72f
Juan Novas <juanignacionovas@gmail.com>
2024-10-22 09:25:13 -0300

    breakpoint() function added

---
+++ bi_functions/breakpoint.py
@@ -0,0 +1,11 @@
+# Calls the Python Debugger in that point of the code
+
+value_a = 10
+value_b = 5
+value_c = value_a + value_b
+
+breakpoint()
+
+print(f"The value of a is {value_a}")
+print(f"The value of b is {value_b}")
+print(f"The value of c is {value_c}")
