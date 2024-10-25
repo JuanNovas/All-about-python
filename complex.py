7e5b6d87ba4f6a097fbcf541f5e6955f2b5094b0
Juan Novas <juanignacionovas@gmail.com>
2024-10-25 16:42:10 -0300

    complex() constructor added

---
+++ complex.py
@@ -0,0 +1,17 @@
+# Converts a single string to a complex number or creates a complex number from real and imaginary parts
+
+complex('+1.23') # Returns (1.23+0j)
+
+complex('-4.5j') # Returns -4.5j
+
+complex('-1.23+4.5j') # Returns (-1.23+4.5j)
+
+complex('\t( -1.23+4.5J )\n') # Returns (-1.23+4.5j)
+
+complex('-Infinity+NaNj') # Returns (-inf+nanj)
+
+complex(1.23) # Returns (1.23+0j)
+
+complex(imag=-4.5) # Returns -4.5j
+
+complex(-1.23, 4.5) # Returns (-1.23+4.5j)
