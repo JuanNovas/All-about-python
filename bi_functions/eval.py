2ad7f833ad67ad6df9624e8fa15e99b43aaf29b8
Juan Novas <juanignacionovas@gmail.com>
2024-10-27 19:31:59 -0300

    eval() function added

---
+++ bi_functions/eval.py
@@ -0,0 +1,7 @@
+# Executes code given as a STR
+# Also accepts 'globals' and 'locals' parameter to especify the scope the expression is capable to access
+# Returns the result of the evaluated expression
+
+
+x = 1
+eval('x+1') # Returns: 2
