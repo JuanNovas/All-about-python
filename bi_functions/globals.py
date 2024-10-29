ec3105bf30943b8179dc7b1ccf163c5045d1b0d9
Juan Novas <juanignacionovas@gmail.com>
2024-10-29 08:53:25 -0300

    globals() function added

---
+++ bi_functions/globals.py
@@ -0,0 +1,15 @@
+# Returns a dictionary including the current module namespace
+
+
+globals() # Returns: 
+                    # {
+                    #     '__name__': '__main__',
+                    #     '__doc__': None,
+                    #     '__package__': None,
+                    #     '__loader__': <_frozen_importlib_external.SourceFileLoader object at 0x000001B8F27249D0>,
+                    #     '__spec__': None,
+                    #     '__annotations__': {},
+                    #     '__builtins__': <module 'builtins' (built-in)>,
+                    #     '__file__': 'path/to/file',
+                    #     '__cached__': None
+                    # }
