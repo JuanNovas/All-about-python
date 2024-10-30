0f6dcf2d8bb118a25851c24665ba4263251acb05
Juan Novas <juanignacionovas@gmail.com>
2024-10-30 11:38:19 -0300

    locals() function added

---
+++ bi_functions/locals.py
@@ -0,0 +1,14 @@
+# Returns a dictionary of the current local symbol table
+
+locals() # Returns: 
+#                {
+#                    '__name__': '__main__',
+#                    '__doc__': None, 
+#                    '__package__': None, 
+#                    '__loader__': <_frozen_importlib_external.SourceFileLoader object at 0x000001CAD03249D0>, 
+#                    '__spec__': None, 
+#                    '__annotations__': {}, 
+#                    '__builtins__': <module 'builtins' (built-in)>, 
+#                    '__file__': 'path/to/file', 
+#                    '__cached__': None
+#                }
