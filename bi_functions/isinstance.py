f9ae7a72cd3bae1e603aa02aca9216b3f46c758f
Juan Novas <juanignacionovas@gmail.com>
2024-10-30 10:24:22 -0300

    isinstance() function added

---
+++ bi_functions/isinstance.py
@@ -0,0 +1,8 @@
+# Returns a BOOL
+# Returns True if the obj is an instance of the second argument
+# Takes an OBJ and a Type or Tuple of types
+
+
+isinstance(4, int) # Returns: True
+
+isinstance('aaa', (int, float)) # Returns: False
