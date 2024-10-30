9d974435f737e10b66da5c7ac32ff76ca44ecc72
Juan Novas <juanignacionovas@gmail.com>
2024-10-30 10:15:26 -0300

    int() constructor added

---
+++ bi_functions/int.py
@@ -0,0 +1,10 @@
+# Creates an INT OBJ
+# Takes a NUMBER, STR or OBJ with the methods: __int__, __index__ or __trunc__
+# Also a custom parameter base that represents the base of the INT
+
+
+int(6) # Returns: 6
+
+int('10') # Returns: 10
+
+int('ten') # Raise: ValueError
