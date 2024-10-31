42f7f37df3179db3372ac46b55edb0ae15e189d3
Juan Novas <juanignacionovas@gmail.com>
2024-10-31 19:28:44 -0300

    map() function added

---
+++ map.py
@@ -0,0 +1,8 @@
+# Returns an iterator that applies a function to every item of the iterable
+# Takes a function and N amount of itetables (N >= 1)
+
+
+map(lambda x : x * 2, [1,2,3,4]) # Returns an iterator that returns: 2, 4, 6, 8
+
+
+map(lambda x, y: x * y, [10,20,30,40], [5,6,7,8]) # Returns an iterator that returns: 50, 120, 210, 320
