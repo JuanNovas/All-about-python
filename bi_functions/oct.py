f4eb8c1e6e03aa69f5bb829122fea50ba7259901
Juan Novas <juanignacionovas@gmail.com>
2024-11-01 09:55:47 -0300

    oct() function added

---
+++ bi_functions/oct.py
@@ -0,0 +1,8 @@
+# oct(x)
+# Converts an INT to an octal STR prefixed with '0o'
+# If x is not an INT it has to have the __index__() method defined
+
+
+oct(8) # Returns: '0o10'
+
+oct(-56) # Returns: '-0o70'
