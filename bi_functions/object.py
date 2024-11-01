a246c693b6fd65042417ee43a9f37073d4d1e761
Juan Novas <juanignacionovas@gmail.com>
2024-11-01 09:52:38 -0300

    object() constructor added

---
+++ bi_functions/object.py
@@ -0,0 +1,6 @@
+# object()
+# Returns a new featureless object
+# Is the base for all clases and has methods that are common to all instances of Python classes
+
+
+obj = object() # Returns and object instance
