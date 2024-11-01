da7fcd0bbdea8f59e40fab726895cd71fb3e8687
Juan Novas <juanignacionovas@gmail.com>
2024-11-01 09:41:08 -0300

    min() function added

---
+++ bi_functions/min.py
@@ -0,0 +1,20 @@
+# Returns the smallest item
+# If one argument is given it should be an Iterable
+# If two or more parameters are given the smallest is returned
+# The optional parameter key tells how the values are going to be trait before ordering and define the min value, takes a function
+# The optional default value only works when an only Iterable is given and returns itself when the iterator is empty
+
+min([1,2,3,4]) # Returns: 1
+
+min([1,2,3,4], default=5) # Returns: 1
+
+min([1,2,3,4], key=lambda x: (x + 2) / x) # Returns: 4
+
+min([], default=2) # Returns: 2
+
+
+min(1,2,3,4) # Returns: 1
+
+min(1,2,3,4, default=5) # Raise: TypeError
+
+min(1,2,3,4, key=lambda x: x - x * 2) # Returns: 4
