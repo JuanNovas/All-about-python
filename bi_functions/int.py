e05832ca452a96e87dd869bf272a9e9cf2978ff5
Juan Novas <juanignacionovas@gmail.com>
2024-11-05 10:55:30 -0300

    int() constructor update

--- bi_functions/int.py
+++ bi_functions/int.py
@@ -1,3 +1,6 @@
+# -- int([x]) --
+# -- int(x, base=10) --
+
 # Creates an INT OBJ
 # Takes a NUMBER, STR or OBJ with the methods: __int__, __index__ or __trunc__
 # Also a custom parameter base that represents the base of the INT
