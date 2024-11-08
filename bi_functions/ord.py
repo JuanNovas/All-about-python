b622f7a489fa0a0e7c2c470cac2e7fa3a05afd03
Juan Novas <juanignacionovas@gmail.com>
2024-11-08 11:56:00 -0300

    ord() function added

---
+++ bi_functions/ord.py
@@ -0,0 +1,13 @@
+# -- ord(c) --
+
+# Given a STR representing one Unicode character, returns the Unicode code point as an INT
+
+ord('a') # Returns: 97
+
+ord('A') # Returns: 65
+
+ord('1') # Returns: 49
+
+ord('@') # Returns: 64
+
+ord(' ') # Returns: 32
