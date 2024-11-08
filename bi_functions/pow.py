5b84efdad944f8a8421cac873b362c2f63264783
Juan Novas <juanignacionovas@gmail.com>
2024-11-08 18:42:42 -0300

    pow() function added

---
+++ bi_functions/pow.py
@@ -0,0 +1,12 @@
+# -- pow(base, exp[, mod]) --
+
+# Returns base to the power exp
+# If mod is present, return base to the power exp, modulo mod
+# Parameters are NUMERIC type
+# Returns a NUMERIC
+
+pow(5, 5) # Returns: 3125
+
+pow(10, -2) # Returns: 0.01
+
+pow(7, 3, 8) # Returns: 7
