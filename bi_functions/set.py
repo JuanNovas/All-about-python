b9fa882b895e482164f50dc773476e03582b76d7
Juan Novas <juanignacionovas@gmail.com>
2024-11-09 17:48:26 -0300

    set() constructor added

---
+++ bi_functions/set.py
@@ -0,0 +1,9 @@
+# -- set([iterable]) --
+
+# Return a new SET object
+# The optional iterable would be the element inside the SET
+
+set() # Returns an empry set
+
+numbers = [1, 2, 3, 4, 5]
+set(numbers) # Returns: {1, 2, 3, 4, 5}
