7ea8edd80588127afbdbc0ed2121292a8573ef3b
Juan Novas <juanignacionovas@gmail.com>
2024-11-10 11:19:14 -0300

    sum() function added

---
+++ bi_functions/sum.py
@@ -0,0 +1,12 @@
+# -- sum(iterable, /, start=0) --
+
+# Sums the start and the items for the iterable
+# Returns the total
+
+num_iterable = [2, 8, 12, 6, -4, 2]
+
+sum(num_iterable) # Returns: 26
+
+sum(num_iterable, start=10) # Returns: 36
+
+sum(num_iterable, start=4.5) # Returns: 30.5
