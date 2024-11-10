7d43660c343898ea2658f5fdb31c794c9f2ac3a9
Juan Novas <juanignacionovas@gmail.com>
2024-11-10 11:14:56 -0300

    str() constructor added

---
+++ bi_functions/str.py
@@ -0,0 +1,9 @@
+# -- str(object='') --
+# -- str(object='', encoding='utf-8', errors='strict') --
+
+# Return a STR version of object 
+
+
+str(23) # Returns: '23'
+
+str(['hello', 'world']) # Returns: "['hello', 'world']"
