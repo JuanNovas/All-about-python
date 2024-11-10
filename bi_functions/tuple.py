e33a9038128d405bad49acd1dee3971fe936c00c
Juan Novas <juanignacionovas@gmail.com>
2024-11-10 11:40:21 -0300

    tuple() constructor added

---
+++ bi_functions/tuple.py
@@ -0,0 +1,10 @@
+# -- tuple([iterable]) --
+
+# Return a tuple, inmutable sequence type
+
+
+tuple() # Returns: ()
+
+tuple([1,3,5]) # Returns: (1,3,5)
+
+tuple(['hello', 'world']) # Returns: ('hello', 'world')
