bd2600941a798d81f39989acd65708d15a336e3b
Juan Novas <juanignacionovas@gmail.com>
2024-11-11 09:30:28 -0300

    comparison type added

---
+++ bi_types/comparison.py
@@ -0,0 +1,10 @@
+# There are eight comparison operations
+
+# < : less than
+# <= : less than or equal
+# > : greater than
+# >= : greater than or equal
+# == : equal
+# != : not equal
+# is : object identity
+# is not : negated object identity
