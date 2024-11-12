667395fdedbb7b46417fcca25ef2de8e39da6acc
Juan Novas <juanignacionovas@gmail.com>
2024-11-12 18:35:15 -0300

    operations added to numeric

---
+++ bi_types/numeric/operations.py
@@ -0,0 +1,16 @@
+# x + y: sum of x and y
+# x - y: difference of x and y
+# x * y: product of x and y
+# x / y: quotient of x and y
+# x // y: floored quotient of x and y
+# x % y: remainder of x / y
+# -x: x negated
+# +x: x unchanged
+# abs(x): absolute value of magnitude of x
+# int(x): x converted to integer
+# float(x): x converted to float
+# complex(re, im): a complex number with real part re, imaginary part im. im defaults to zero
+# c.conjugate(): conjugate of the complex number c
+# divmod(x, y): the pair (x // y, x % y)
+# pow(x, y): x to the power y
+# x ** y: x to the power y
