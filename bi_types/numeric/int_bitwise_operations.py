c4948f2accd34ec9ae6cad227fa2a6feef830af1
Juan Novas <juanignacionovas@gmail.com>
2024-11-15 22:09:53 -0300

    bitwise operations for int added

---
+++ bi_types/numeric/int_bitwise_operations.py
@@ -0,0 +1,6 @@
+# x | y: bitwise or of c and y
+# x ^ y : bitwise exclusive or of x and y
+# x & y: bitwise and of x and y
+# x << n: x shifted left by n bits
+# x >> n: x shifted right by n bits
+# ~x : the bits of x inverted
