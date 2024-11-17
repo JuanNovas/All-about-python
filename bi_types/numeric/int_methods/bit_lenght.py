43e08a8d47eb90d3d95223caa22e8c8b1293530b
Juan Novas <juanignacionovas@gmail.com>
2024-11-17 19:19:23 -0300

    int method bit_length() added

---
+++ bi_types/numeric/int_methods/bit_lenght.py
@@ -0,0 +1,11 @@
+# -- int.bit_lenght() --
+
+# Returns the number of bits necesary to represent an integer in binary, excluding the sign and leading zeros
+
+n = 10
+
+n.bit_length() # Returns: 4
+
+n = -37
+
+n.bit_length() # Returns: 6
