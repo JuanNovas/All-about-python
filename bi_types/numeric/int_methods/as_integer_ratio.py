2641908a82d63e169a0667683e3c3d4333a87d4d
Juan Novas <juanignacionovas@gmail.com>
2024-11-22 11:26:08 -0300

    as_integer_ratio method added

---
+++ bi_types/numeric/int_methods/as_integer_ratio.py
@@ -0,0 +1,6 @@
+# -- int.as_integer_ratio() -> TUPLE --
+
+# Return a pair of integer whose ratio is exactly equal to the original and with a positive denominator
+
+n = 5
+n.as_integer_ratio() # Returns: (5, 1)
