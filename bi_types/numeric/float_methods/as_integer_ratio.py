f7bf0ca0ac5963600afde13566c6f9155be10aaa
Juan Novas <juanignacionovas@gmail.com>
2024-11-23 18:48:12 -0300

    as_integer_ratio() float method added

---
+++ bi_types/numeric/float_methods/as_integer_ratio.py
@@ -0,0 +1,6 @@
+# -- float.as_integer_ratio() -> TUPLE --
+
+# Return a pair of integer whose ratio is exactly equal to the original and with a positive denominator
+
+n = 5.4
+n.as_integer_ratio() # Returns: (3039929748475085, 562949953421312)
