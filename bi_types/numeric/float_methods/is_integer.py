2840eabad21c74aec695fbb5af0116aed60d50c8
Juan Novas <juanignacionovas@gmail.com>
2024-11-24 16:24:09 -0300

    is_integer float method added

---
+++ bi_types/numeric/float_methods/is_integer.py
@@ -0,0 +1,9 @@
+# -- float.is_integer() -> BOOL --
+
+# Returns True is the float is finite with integral value
+
+f = -2.0
+f.is_integer() # Returns: True
+
+f = 3.2
+f.is_integer() # Returns: False
